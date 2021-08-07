c233c8b18813d311f5bb0f3dc4899f4ee2c03776
Divyanshu Shah <divyanshushah89@gmail.com>
2021-08-07 16:51:58 +0530

    Inital Commit

---
+++ PromotionsApp/Promotion.Core/Rules/IRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionsApp.Promotion.Core.Rules
+{
+    class IRule
+    {
+    }
+}
